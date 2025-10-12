24535a5509b0d32b17e036a67a5a9e64f5725628
USUARIO <USUARIO@192.168.0.12>
2025-10-12 01:32:22 -0300

    CARGA 12/10 - 01:32

--- src/main/java/isi/deso/Presentacion/Main.java
+++ src/main/java/isi/deso/Presentacion/Main.java
@@ -18,11 +18,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
+        /* LO DEJO COMENTADO PARA QUE PUEDAN PROBAR SIN INICIAR SESION
         //LECTURA DE TXT CON CONSERJES
         Archivo a = new Archivo();
         
         String arch = a.leerTxt("D:\\USUARIO\\Documents\\txt\\usuariosCargados.txt");
-        System.out.println(arch);
         
         String[] datosUsers = arch.split(" ");
         List<Usuario> listaDeUsuarios = new ArrayList<>();
@@ -40,52 +40,22 @@ public class Main {
 
         }
         
-        System.out.println("\n--- Lista actual de usuarios ---");
-        servicio.listarTodos().forEach(System.out::println);
-        //ACA TERMINA - PASAR A UNA NUEVA CLASE
+        // PARA VER LA LISTA - BORRAR DSP
+       // System.out.println("\n--- Lista actual de usuarios ---");
+       // servicio.listarTodos().forEach(System.out::println);
         
-        /*
+        //PANTALLA PPAL
+        System.out.println("BIENVENIDO");
+        System.out.println("Ingrese sus datos para continuar\n");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresar nombre de usuario: ");
+        String nombreUs = scanner.nextLine();
+        System.out.println("Ingresar contrasena: ");
+        String contrasenia = scanner.nextLine();
         
-        int opcionA; //Es el que ingresa en el switch de opciones
-        
-        //CARGA DEL USUARIO GENERAL - NO VA, CORREGIR
-        Usuario user089 = new Usuario("Agustin", "Colli", "AColli089", "Tate1989");
-        servicio.registrarUsuario(user089);
-        
-        do{
-            System.out.println("\nSeleccionar una opcion: ");
-            System.out.println("\t1. Iniciar Sesion");
-            System.out.println("\t2. Agregar Usuario nuevo");
-            System.out.println("Eleccion: ");
-            opcionA = scanner.nextInt();
-        
-            switch(opcionA){
-                case 1: 
-                    System.out.println("Aca iria la creacion de usuario");
-                    break;
-                case 2:
-                    scanner.nextLine();
-                    System.out.println("\nIngrese su nombre: ");
-                    String nombre = scanner.nextLine();
-                    System.out.println("\nIngrese su apellido: ");
-                    String apellido = scanner.nextLine();
-                    System.out.println("\nIngrese una ID: ");
-                    String nombreUs = scanner.nextLine();
-                    System.out.println("\nIngrese una contraseña: ");
-                    String contrasenia = scanner.nextLine();
-                    Usuario user001 = new Usuario(nombre, apellido, nombreUs, contrasenia);
-                   
-                    System.out.println("\nUsuario cargado correctamente");
-                    System.out.println(user001);
-                    
-                    servicio.registrarUsuario(user001);
-                    
-                    // 3. (Opcional) Verificamos el contenido de la lista para confirmar
-            System.out.println("\n--- Lista actual de usuarios ---");
-            servicio.listarTodos().forEach(System.out::println);
-
-            }
-        } while(opcionA > 2);*/
+        if(servicio.validarUser(nombreUs, contrasenia)){
+            System.out.println("Usuario validado");
+        } else { System.out.println("Usuario no encontrado");}
+        */
     }
 }
