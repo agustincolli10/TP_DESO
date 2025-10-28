e8f139c82adeba36522a4f6461a406b38dde2f6d
maximom29 <150258297+maximom29@users.noreply.github.com>
2025-10-28 17:34:58 -0300

    Update Huesped.java

--- src/main/java/isi/deso/Modelo/Huesped.java
+++ src/main/java/isi/deso/Modelo/Huesped.java
@@ -5,5 +5,10 @@ public class Huesped extends Persona {
   public Huesped(String apellido, String nombres, TipoDocumento tipo, String numero){
     this.apellido=apellido; this.nombres=nombres; this.tipoDocumento=tipo; this.numeroDocumento=numero;
   }
+    public String getApellido() { return apellido; }
+  public String getNombres()  { return nombres; }
+  public TipoDocumento getTipoDocumento() { return tipoDocumento; }
+  public String getNumeroDocumento() { return numeroDocumento; }
   @Override public String toString(){ return apellido + ", " + nombres + " ("+idNatural()+")"; }
 }
+
