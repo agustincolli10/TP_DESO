648f15606064a26705a18a60dfc006fd3db09d6e
maximom29 <150258297+maximom29@users.noreply.github.com>
2025-10-29 12:10:36 -0300

    Update UsuarioServicio.java

--- src/main/java/isi/deso/Negocio/UsuarioServicio.java
+++ src/main/java/isi/deso/Negocio/UsuarioServicio.java
@@ -1,55 +1,31 @@
-
 package isi.deso.Negocio;
 
 import isi.deso.DAO.UsuarioDAO;
-import isi.deso.DAO.UsuarioDaoImp;
+import isi.deso.DAO.UsuarioDAOImpFile; //  usar archivo
 import isi.deso.Modelo.Usuario;
 import java.util.List;
 
 public class UsuarioServicio {
-    private UsuarioDAO usuarioDao = UsuarioDaoImp.getInstance();
+    private final UsuarioDAO usuarioDao;
+
+    public UsuarioServicio() {
+        this.usuarioDao = new UsuarioDAOImpFile();
+    }
+  
+    public UsuarioServicio(UsuarioDAO dao) {
+        this.usuarioDao = dao;
+    }
 
     public void registrarUsuario(Usuario nuevoUsuario) {
         usuarioDao.guardarUsuario(nuevoUsuario);
     }
-    
-    // Método para verificar que se guardó
+
     public List<Usuario> listarTodos() {
         return usuarioDao.listaCompUser();
     }
-    
+
     public Boolean validarUser (String nombreUs, String contra){
-      Boolean valido = false;
-        
-      List<Usuario> listaUsuarios = usuarioDao.listaCompUser();
-      
-      int i=0;
-      
-      while (i < listaUsuarios.size() && valido == false){
-          Usuario userTemp = listaUsuarios.get(i);
-          
-          if((userTemp.getNUsuario().equals(nombreUs)) && (userTemp.getContrasenia().equals(contra))){
-              valido = true;
-              System.out.println("Bienvenido " + userTemp.getNombre() + " " + userTemp.getApellido());
-          }
-          
-          i++;
-      }
-      
-      return valido;
+      return usuarioDao.listaCompUser().stream()
+              .anyMatch(u -> u.getNUsuario().equals(nombreUs) && u.getContrasenia().equals(contra));
     }
-    
-    /*public Usuario validarUsuario(String nombreUs, String contrasenia) {
-        Usuario usuarioEncontrado = usuarioDao.buscarPor(nombreUs);
-
-        if (usuarioEncontrado == null) {
-            return null; // El usuario no existe, la validación falla.
-        }
-
-        if (usuarioEncontrado.getContrasenia().equals(contrasenia)) {
-            return usuarioEncontrado; // ¡Éxito! Las credenciales son correctas.
-        } else {
-            return null; // La contraseña es incorrecta, la validación falla.
-        }
-    }*/
 }
